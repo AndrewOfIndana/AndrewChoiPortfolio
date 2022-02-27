da35977bca4e8987b86f11836c07b53d1aff1d5d
Andy-of-Indiana <99208998+AndrewOfIndana@users.noreply.github.com>
2022-02-27 03:47:30 -0500

    Essay and Unreal Level

--- app/model.js
+++ app/model.js
@@ -34,10 +34,10 @@ var aProject = `
     <p>Proof of playtesting by another group</p>
   </div>
 </div>
-<a href="assets/aMIDTERM Design Document.docx" download>Download the alpha design document</a>
-<a href="assets/aMIDTERM Andrew Choi Personal Reflection.docx" download>Download my personal reflection during alpha</a>
-<a href="assets/aFINAL Design Document.docx" download>Download the beta design document (final)</a>
-<a href="assets/aFinal Andrew Choi Personal Reflection.docx" download>Download my personal reflection during beta</a>
+<a href="assets/aMIDTERM Design Document.docx" download>Download to alpha design document</a>
+<a href="assets/aMIDTERM Andrew Choi Personal Reflection.docx" download>Download to my personal reflection during alpha</a>
+<a href="assets/aFINAL Design Document.docx" download>Download to beta design document (final)</a>
+<a href="assets/aFinal Andrew Choi Personal Reflection.docx" download>Download to my personal reflection during beta</a>
 </div>
 `;
 var bProject = `
@@ -45,43 +45,59 @@ var bProject = `
 <div class="projectsRow">
   <div class="projectsWords">
     <h3>Unreal 4 Level</h3>
-    <p>This is a level made in Unreal 4 by me, the level takes place in a destroyed city. The level feature many interactable including a teleporting volumn that will move the player back to the beggining of the level, as well as light that trun on when the player gets close.</p>
+    <p>This is a level made in Unreal 4 by me, the level takes place in a destroyed city. The level feature many interactable including a teleporting volumn that will move the player back to the beggining of the level, as well as light that turn on when the player gets close.</p>
   </div>
   <div class="projectsMedia">
     <div class="bMedia1"></div>
-    <p>Insert disc</p>
+    <p>A upward view looking towards a the highest skyscraper in the city.</p>
   </div>
 </div>
 <div class="projectsRow">
   <div class="projectsMedia">
     <div class="bMedia2"></div>
-    <p>Insert disc</p>
+    <p>A broken bridge between two rooftop buildings, along with more the cityscape in the background</p>
   </div>
   <div class="projectsMedia">
     <div class="bMedia3"></div>
-    <p>Insert disc</p>
+    <p>A upward view of more buildings in the park section of the city</p>
   </div>
 </div>
 <div class="projectsRow">
   <div class="projectsMedia">
     <div class="bMedia4"></div>
-    <p>Insert disc</p>
+    <p>A downward shot of a bridge, if the player falls between the gaps they will teleport back to the start</p>
   </div>
   <div class="projectsMedia">
     <div class="bMedia5"></div>
-    <p>Insert disc</p>
+    <p>A hallway with lights, when the player reaches this point all of the light will activate from being off</p>
   </div>
 </div>
+<a href="assets/bLevelDesignDocumentChoi.docx" download>Download to the level design document</a>
 </div>
 `;
 var cProject = `
 <div class="projectsTemplate">
 <div class="projectsRow">
-  <div class="projectsWords">
+  <div class="projectsEssay">
     <h3>My Favorite Game</h3>
-    <p>Lorem ipsum</p>
+    <p style="text-indent: 75px;">
+      Before I start, I want to clarify that what I consider to be the best game and my favorite game are completely different from each other. To explain, what I consider to be the best game is a game that is influential in providing enjoyment, community, and profit. In my own opinion, one of the best games of all time is Minecraft, which provides a near limitless amount of content, from both the developers, and mod from the community; has a bustling fandom of players, each with different interests, from the speed runners, to the builders, to the average causal players; and at this moment is the number one bestselling game in the world. Many would consider Minecraft to be an absolute success and is continued to be talked about even to this day. However to me, though I enjoy playing Minecraft when I get the chance, I wouldn't consider it to be my favorite game, this is because some game to me is more enjoyable than Minecraft, but I wouldn't consider these to be better. To get right to it, my favorite game is Pikmin 2 for the GameCube and Wii. Unlike Minecraft, Pikmin 2 isn't a game for everyone, and only appeals to a certain audience; it has a small community of players for the franchise and even smaller fanbase for the game due to how polarizing the 2nd entry is for the overall community; and isn't a massive seller compared to Nintendo's other more successful franchisees. Despite all of this, Pikmin 2 is my favorite game in the series and of all time, but to go further into I need explain what Pikmin 2 is and what makes it so fun.
+    </p>
+    <p style="text-indent: 75px;">
+      Pikmin 2 is the 2nd game in the Pikmin franchise, during the events of the 1st game, captain Olimar, the protagonist of both the 1st and 2nd game, returns home after crashing and repairing his ship on Earth. Upon setting foot back to his home, he is told that his company Hocotate Freight is in massive debt and has to sell his ship to pay off a small portion of the debt. After witnessing his ship get sold Olimar would then drop a bottle cap he was carrying as a souvenir for his family from Earth. A nearby ship Hocotate ship would then scan the bottle cap and find that it is worth a good amount of money, upon learning this the president of Hocotate Freight would then order Olimar and new hire Louie to go back to Earth and carry back the treasures of Earth to pay back the debt. Upon reaching Earth, Olimar and Louie would then reencounter, the Pikmin, a species of humanoid plant creatures that have helped Olimar repair his ship in the 1st game. The Pikmin are grown from an onion, a mothership or home for the Pikmin, which the Pikmin are sprouted from and plucked, from there they rely on the captains to help them fight, carry items back home, and build and move obstacles. In the game the captains must use Pikmin to do actions in the game, the captain can whistle Pikmin over to them, and throw Pikmin at the cursor, the captain can also swarm their Pikmin to deal with small and easy to kill enemies. When the Pikmin kill their enemy they will carry the body back to the onion, in which the onion will absorb the carcass and produce more Pikmin seeds, from there the captains can pluck the seed to bolster their Pikmin army. The Pikmin can also carry back treasure to the ship as well as other items. Eventually the player will encounter other types of Pikmin each with their own capabilities and uses. For example yellow Pikmin are resistant to lightning and can be thrown higher than most Pikmin, while purple Pikmin are really good fighters, and can carry 10 times their own weight. The player must juggle what types of Pikmin they want to carry and what they will do throughout the in-game day. This should go over the basic of the story and gameplay of Pikmin 2, but what makes the game fun? 
+    </p>
+    <p style="text-indent: 75px;">
+      In the Pikmin series, the player only has a certain amount of time to do anything before the day ends, when the day ends the Pikmin and the Captain return to the ship and fly into the atmosphere to escape the nocturnal creatures and any Pikmin that isn't whistles over or near the ship and onions will be left behind and eaten. The fun comes from trying to get as much done within the amount of time they are given each day, finding ways to cut time, finding the best route, and planning out what needs to be done. In some instances I was able to shorten what would've taken most causal player three in game days to accomplish, to one in game day. All three Pikmin games has this game mechanic ingrained into its gameplay, and in my opinion, it is more refined in the 3rd game, where in that game the player can go back to an earlier day at any point. In that case, why do I prefer Pikmin 2 if Pikmin 3 has the more refined gameplay? To answer that, in Pikmin 2 the player will encounter holes in the ground, these are caves, which are essentially the dungeons. In the caves, player must brave though randomly generated levels each with their own set of enemies and layout, the amount of time the player can spend in a cave is limitless and the player can use whatever method to finish the level. The caves are what caused the polarizing of the fandom about Pikmin 2, some player absolutely despise the caves and think they disrupt the natural gameplay of the overworld, where it is all about plotting and getting as much done within the amount of time of the day, while other love the caves, where it is all about testing the player's exploration, combat, and patience. In my opinion I love the caves, I love how each cave is distinct and has its own theme, I love how the game forces you to slow down and think critically on how you should handle each and every challenge, and I love how the caves reward exploration where in the overworld exploration is discouraged if the player wants to make the most of what time they have. I completely understand why some people would dislike the caves with some caves going on for ten sublevels and more, but I think the caves offer a different set of challenges unique to the gameplay flow of the overworld. However the Pikmin 2 offers more than just the gameplay.
+    </p>
+    <p style="text-indent: 75px;">
+      Another reason, why I like Pikmin 2 compared to other games and even the other installments in the series, is that Pikmin 2 has a lot of charm and personality that most games lack, to give an example in the map selection screen, if the player presses the side button, they will be brought to the Piklopedia, a gallery of each enemy in the game with notes from both captains, Olimar's notes being about the biological behaviors of the enemy as well as its genus and species name, while Louie's notes are about how to cook this creature and serve it as a meal. The developer did not have to put this into the game, as the gameplay and presentation was more than enough, however by adding this, the world of Pikmin feels alive and immersive, the enemy that the player fights aren't just enemy but are animals that have their own behaviors and habitats. Another example is when the player finishes a day, they will be brought to the results screen which detail how many Pikmin was grown, or lost, how much money was earned, and most importantly the email they can receive. The email can range from being from the president, scolding Olimar for working hard enough, Olimar's wife and family, updating Olimar on what is happening, Louie's grandma, saying how much she misses him, and even scam mail, with enjoyable flavor text. From these emails the player can learn more about the characters and world, like how the president accidently mistook loan shark from insurance, or how the president's wife would restructure the entirety of the company after the president ran to live under a bridge, small little stories like that are peppered within the emails and help the player feel immersed in the game they are playing.
+    </p>
+    <p style="text-indent: 75px;">
+      To finally conclude, I wouldn't consider Pikmin 2 to be the best game in my opinion, not everyone will like it as much as I would. Though I have not mentioned it, the game has a wide variety of problems and things that even I don't like, but the enjoyment and charm that the game has given me far exceeds and of those problems.
+    </p>
   </div>
 </div>
+<a href="assets/cMyFavoriteGame.docx" download>Download the full document</a>
 </div>
 `;
 var eProject = `
